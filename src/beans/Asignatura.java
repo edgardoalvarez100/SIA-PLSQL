6170a8296901e22b977c9c28eab05ac47bb9c2bc
Edgardo Alvarez <edgardoalvarez100@gmail.com>
2015-04-10 16:19:06 -0500

    Actualizar Asignaturas completada

--- src/beans/Asignatura.java
+++ src/beans/Asignatura.java
@@ -13,4 +13,70 @@ public class Asignatura {
     int horas_practicas;
     int horas_independientes;
     String tipo;
+
+    public int getIdAsigntaura() {
+        return idAsigntaura;
+    }
+
+    public void setIdAsigntaura(int idAsigntaura) {
+        this.idAsigntaura = idAsigntaura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public int getHoras_teoricas() {
+        return horas_teoricas;
+    }
+
+    public void setHoras_teoricas(int horas_teoricas) {
+        this.horas_teoricas = horas_teoricas;
+    }
+
+    public int getHoras_practicas() {
+        return horas_practicas;
+    }
+
+    public void setHoras_practicas(int horas_practicas) {
+        this.horas_practicas = horas_practicas;
+    }
+
+    public int getHoras_independientes() {
+        return horas_independientes;
+    }
+
+    public void setHoras_independientes(int horas_independientes) {
+        this.horas_independientes = horas_independientes;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
 }
--- src/dao/AsignaturaDao.java
+++ src/dao/AsignaturaDao.java
@@ -1,10 +1,13 @@
 package dao;
 
+import beans.Asignatura;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import oracle.jdbc.OracleCallableStatement;
 import oracle.jdbc.OracleTypes;
 
 /**
@@ -13,21 +16,73 @@ import oracle.jdbc.OracleTypes;
  */
 public class AsignaturaDao extends DataBaseOracle {
 
+    ResultSet rs;
     Connection con = null;
     CallableStatement cst;
 
-    public String actualizar() {
+    public Asignatura buscarPorId(int codigo) {
+        Asignatura as = null;
         con = conectar();
-        String sql = "{call }";
+        String sql = "{call BUSCAR_ASIGNATURA(?,?)}";
         try {
             cst = con.prepareCall(sql);
-            cst.setString(sql, sql);
+            cst.setInt(1, codigo);
+            cst.registerOutParameter(2, OracleTypes.INTEGER);
+            cst.executeUpdate();
+            rs = ((OracleCallableStatement) cst).getCursor(2);
+            while(rs.next()){
+                as = new Asignatura();
+                as.setIdAsigntaura(codigo);
+                as.setCreditos(rs.getInt("asi_creditos"));
+                as.setHoras_independientes(rs.getInt("asi_horas_independientes"));
+                as.setHoras_practicas(rs.getInt("asi_horas_practicas"));
+                as.setHoras_teoricas(rs.getInt("asi_horas_teoricas"));
+                as.setTipo(rs.getString("asi_tipo"));
+                as.setNombre(rs.getString("asi_nombre"));                
+            }
 
         } catch (SQLException ex) {
             Logger.getLogger(AsignaturaDao.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                desconectarBD(con);
+            } catch (SQLException ex) {
+                Logger.getLogger(AsignaturaDao.class.getName()).log(Level.SEVERE, null, ex);
+
+            }
+        }
+        return as;
+    }
+
+    public int actualizar(int codigo, String descripcion, int creditos,
+            int hr_teorica, int hr_practica, int hr_indepen, String tipo) {
+        int respuesta = 0;
+        con = conectar();
+        String sql = "{call GUARDAR_ASIGNATURA(?,?,?,?,?,?,?)}";
+        try {
+            cst = con.prepareCall(sql);
+            cst.setInt(1, creditos);
+            cst.setString(2, descripcion);
+            cst.setInt(3, creditos);
+            cst.setInt(4, hr_teorica);
+            cst.setInt(5, hr_practica);
+            cst.setInt(6, hr_indepen);
+            cst.setString(7, tipo);
+            cst.registerOutParameter(8, OracleTypes.INTEGER);
+            cst.executeUpdate();
+            respuesta = cst.getInt(8);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(AsignaturaDao.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                desconectarBD(con);
+            } catch (SQLException ex) {
+                Logger.getLogger(AsignaturaDao.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
 
-        return null;
+        return respuesta;
     }
 
     public int guardar(String descripcion, int creditos,
