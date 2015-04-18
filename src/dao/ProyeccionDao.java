d31d30c3548497e3f8e3c99f16d82f9a1d7131b1
Edgardo Alvarez <edgardoalvarez100@gmail.com>
2015-04-18 09:28:11 -0500

    agregado funcion guardar

--- src/dao/ProyeccionDao.java
+++ src/dao/ProyeccionDao.java
@@ -1,27 +1,59 @@
-
 package dao;
 
 import beans.Proyeccion;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import oracle.jdbc.OracleTypes;
 
 /**
  *
  * @author EAlvarez
  */
-public class ProyeccionDao extends DataBaseOracle{
-    
-public int guardar (Proyeccion proyeccion){
-    return 0;
-    
-}
- 
-public int actualizar(Proyeccion proyeccion){
-    return 0;
-    
-}
+public class ProyeccionDao extends DataBaseOracle {
 
-public int eliminar(int idProyeccion){
-    return 0;
-    
-}
+    ResultSet rs;
+    Connection con = null;
+    CallableStatement cst;
+    String sql = "";
+
+    public int guardar(int cod_matricula, int cod_asignatura) {
+        int respuesta = 0;
+        con = conectar();
+        sql = "{call GUARDAR_PROYECCION(?,?,?)}";
+        try {
+            cst = con.prepareCall(sql);
+            cst.setInt(1, cod_asignatura);
+            cst.setInt(2, cod_matricula);
+            cst.registerOutParameter(3, OracleTypes.INTEGER);
+            cst.executeUpdate();
+            respuesta = cst.getInt(3);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(AsignaturaDao.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                desconectarBD(con);
+            } catch (SQLException ex) {
+                Logger.getLogger(AsignaturaDao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return respuesta;
+
+    }
+
+    public int actualizar(Proyeccion proyeccion) {
+        return 0;
+
+    }
+
+    public int eliminar(int idProyeccion) {
+        return 0;
+
+    }
 
 }
